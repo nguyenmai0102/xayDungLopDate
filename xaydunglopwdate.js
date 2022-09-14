8d6826a15f1598d962892c5d3d4aa765c4a29751
unknown <nguyenmai0102.meo@gmail.com>
2022-09-14 17:20:59 +0700

    noi dung

---
+++ xaydunglopwdate.js
@@ -0,0 +1,27 @@
+class day{
+date; moon; year
+
+    constructor(date, moon, year) {
+    this.date=date;
+    this.moon=moon;
+    this.year=year;
+    }
+    setDate(date){
+     this.date=date
+    }
+    getDate(){
+    return this.date
+    }
+    setMoon(moon){
+    this.moon=moon
+    }
+    getMoon(){
+    return this.moon
+    }
+    setYear(year){
+    this.year = year
+    }
+    getYear(){
+    return this.year
+    }
+}
